e023ee448111186d45f1609b9c449135c56a6dff
Vinícius Venturini <viniciusventurini47@gmail.com>
2021-07-19 14:06:24 -0300

    update: script de instalação completo e arquivo de log adicionado

--- addurl.py
+++ addurl.py
@@ -1,20 +1,37 @@
+import sys
 import mysql.connector
+import threading
+from _thread import *
 
-mydb = mysql.connector.connect(
-  host="",
-  user="",
-  password="",
-  database=""
-)
+def exe(sql):
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="DNS",
+      password="password",
+      database="dns"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute(sql)
+    mydb.commit()
 
-while 1:
-	try:
-		newLine = input()
-	except EOFError:
-		break
-	mycursor = mydb.cursor()
-	val = newLine.rstrip()
-	sql = "INSERT INTO <insira o nome da sua tabela aqui> (<insira o campo de url aqui>) VALUES (\""
-	sql += val+"\")"
-	mycursor.execute(sql)
-	mydb.commit()
+file = open("blocklist.list", "r")
+data = file.read()
+data = data.split("\n")
+file.close()
+i = 0
+tam = len(data)
+
+while i < tam:
+    sql = "INSERT INTO urls VALUES "
+    subtam = i + 100000
+    while i < subtam and i < tam:
+        if data[i] != '':
+            sql += "('"+data[i]+"'),"
+        i += 1
+    exe(sql[0:len(sql) - 1] + ';')
+    percent = (i * 100) / tam
+    format_per = "{:.2f}".format(percent)
+    print(str(format_per) + "% done...", end="\r")
+
+sys.stdout.write("\033[K")
+print("Finished")
--- install.sh
+++ install.sh
@@ -2,17 +2,24 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python3.8 -y
+sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
-pip install -r requirements.txt
+sudo python3 -m pip install -r requirements.txt
 sudo apt-get install mysql-server -y
 sudo systemctl enable mysql
 sudo systemctl start mysql
 
-readonly Q1="CREATE DATABASE IF NOT EXISTS urls;"
-readonly Q2="CREATE USER IF NOT EXISTS 'DNS'@'localhost' IDENTIFIED BY 'password';"
+readonly Q1="CREATE DATABASE IF NOT EXISTS dns;"
+readonly Q2="CREATE USER IF NOT EXISTS 'DNS'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
 readonly Q3="GRANT ALL PRIVILEGES ON *.* TO 'DNS'@'localhost';"
 readonly Q4="FLUSH PRIVILEGES;"
-readonly QUERY="${Q1}${Q2}${Q3}${Q4}"
+readonly Q5="USE dns;"
+readonly Q6="CREATE TABLE IF NOT EXISTS urls (url varchar(255), Primary Key (url));"
+readonly Q7="set global max_connections = 100000;"
+readonly Q8="set global max_allowed_packet = 67108864;"
+readonly QUERY="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}${Q8}"
 
 sudo mysql -u root -p -e "$QUERY"
+
+sudo python3 addurl.py
+touch server.log
